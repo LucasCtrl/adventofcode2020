577d369f9b8d08e5daba0a05d6a673fc70d72738
LucasCtrl <lucasalt@protonmail.com>
2020-12-03 18:30:30 +0100

    :sparkles: Completed day 1 - part 2

--- day1/solver.js
+++ day1/solver.js
@@ -7,20 +7,25 @@ let years = {}
 const yearSum = (data) => {
   data.forEach((year1) => {
     data.forEach((year2) => {
-      if (year1 + year2 == 2020) {
-        years = {
-          year1: year1,
-          year2: year2,
+      data.forEach((year3) => {
+        if (year1 + year2 + year3 == 2020) {
+          years = {
+            year1: year1,
+            year2: year2,
+            year3: year3,
+          }
+          console.log(years)
         }
-      }
+      })
     })
   })
 }
 
-const multiplyYears = (year1, year2) => {
-  return year1 * year2
+const multiplyYears = (year1, year2, year3) => {
+  return year1 * year2 * year3
 }
 
 yearSum(data)
+console.log(years)
 
-console.log(multiplyYears(years.year1, years.year2))
+console.log(multiplyYears(years.year1, years.year2, years.year3))
