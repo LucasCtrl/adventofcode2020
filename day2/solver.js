a94c836df8dcea0c30dc14dfa86d3788d1bef0a2
LucasCtrl <lucasalt@protonmail.com>
2020-12-03 18:59:46 +0100

    :sparkles: Completed day 2 - part 2

--- day2/solver.js
+++ day2/solver.js
@@ -1,8 +1,9 @@
 const data = require('./input')
 
-let validPassword = 0
+let validPasswordPart1 = 0
+let validPasswordPart2 = 0
 
-const testPassword = (data) => {
+const testPasswordPart1 = (data) => {
   data.forEach((password) => {
     let [qty, letter, pass] = password.split(' ')
     let [minQty, maxQty] = qty.split('-')
@@ -12,9 +13,28 @@ const testPassword = (data) => {
 
     let letters = passArray.filter((l) => l == letter.slice(0, -1))
     console.log(letters, letters.length)
-    if (letters.length >= minQty && letters.length <= maxQty) validPassword++
+    if (letters.length >= minQty && letters.length <= maxQty) validPasswordPart1++
   })
 }
 
-testPassword(data)
-console.log(validPassword)
+const testPasswordPart2 = (data) => {
+  data.forEach((password) => {
+    let [pos, letter, pass] = password.split(' ')
+    let [pos1, pos2] = pos.split('-')
+    let passArray = pass.split('')
+
+    let letter1 = passArray[pos1 - 1]
+    let letter2 = passArray[pos2 - 1]
+
+    if (letter1 == letter.slice(0, -1) && letter2 == letter.slice(0, -1)) return
+    else if (letter1 == letter.slice(0, -1) || letter2 == letter.slice(0, -1)) {
+      console.log(true, pos, letter, letter1, letter2)
+      validPasswordPart2++
+    }
+  })
+}
+
+testPasswordPart1(data)
+testPasswordPart2(data)
+console.log(validPasswordPart1)
+console.log(validPasswordPart2)
